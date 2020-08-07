7a34ea8038e17ccd8d35ddbd75ba15118fc9e868
Trần Nhật Nam <namtn@chipo.vn>
2020-08-07 23:29:13 +0700

    update

--- app/app.js
+++ app/app.js
@@ -113,13 +113,13 @@ angular.module('myApp', [
         }])
     .constant('Constants', {
         'GLOGIN': {
-            'CLIENT_ID': '785259876868-92a13mgl9gpe2i0knu2budtum9ov6edi.apps.googleusercontent.com',
-            'CLIENT_SECRET': 'x_GleC5WyJIb5-GUmIY0Csua',
+            'CLIENT_ID': 'YOUR_CLIENT_ID',
+            'CLIENT_SECRET': 'YOUR_CLIENT_SECRET',
             'SCOPE_GOOGLE': 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
         },
         'GCAPTCHA': {
-            'SITE_KEY': '6Lfxp7sZAAAAAGMLFY8CpWZ_pl1sqBH8jxaYfv2h',
-            'SECRET_KEY': '6Lfxp7sZAAAAADa5_Io1L6yFW9OOvYkPmRBab52M'
+            'SITE_KEY': 'YOUR_SITE_KEY',
+            'SECRET_KEY': 'YOUR_SECRET_KEY'
         }
     })
 ;
